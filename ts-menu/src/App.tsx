cdbfae6c9f4e94409c07fef66f059cf3f0805056
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-12-04 14:53:30 +1100

    Project Setup and adding project folder

---
+++ ts-menu/src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Menu from './components/Menu';
+
+export default function App() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
---
+++ ts-menu/src/components/LoginForm/index.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const LoginForm:React.FC<any> = (props: any) => {
+  return (
+    <div className="loginContainer">
+      Login Form
+    </div>
+  );
+}
