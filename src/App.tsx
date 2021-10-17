d6c40f01c5a42969cfec34e8fdcbc16e97471f77
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-10-17 21:56:32 +1100

    refactor: refactor project entrance and related css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import * as React from "react";
-import HomePage from "./HomePage";
-import "antd/dist/antd.css";
+import { AuthenticatedApp } from "./AuthenticatedApp";
+import React from "react";
+import "./App.css";
+import { UnauthenticatedAPP } from "./unauthenticatedApp";
+import { useAuth } from "./context/auth-context";
+import { ErrorBoundary } from "./components/errorBoundary";
+import { FullPageErrorFallback, FullPageLoading } from "./components/lib";
 
 export default function App() {
-  return <HomePage />;
+  const { user } = useAuth();
+
+  return (
+    <div className="App">
+      <ErrorBoundary fallbackRender={FullPageErrorFallback}>
+        <React.Suspense fallback={<FullPageLoading />}>
+          {user ? <AuthenticatedApp /> : <UnauthenticatedAPP />}
+        </React.Suspense>
+      </ErrorBoundary>
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import * as React from "react";
-import { render } from "react-dom";
+import ReactDOM from "react-dom";
+import "antd/dist/antd.css";
+import { AppProviders } from "../src/context/index";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
