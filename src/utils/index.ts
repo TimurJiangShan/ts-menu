de5b18074939f212208358bb6afef3f7d9f9434a
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-10-17 19:52:56 +1100

    feat: add util functions

---
+++ src/utils/index.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState, useRef } from "react";
+
+interface KeyValueObject {
+  [key: string]: unknown;
+}
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
+
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === "";
+
+export const useDebounce = <V>(value: V, delay?: number) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDocumentTitle = (title: string, keepOnUnmount = true) => {
+  const oldTitle = useRef(document.title).current;
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  useEffect(() => {
+    return () => {
+      if (!keepOnUnmount) {
+        document.title = oldTitle;
+      }
+    };
+  }, [title, keepOnUnmount]);
+};
+
+export const cleanObject = (object: KeyValueObject) => {
+  // Object.assign({}, object)
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isVoid(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
