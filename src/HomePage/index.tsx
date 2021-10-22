f65ec57a3e7e24a212523bce92bc6610adc91f7e
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2021-10-22 20:57:59 +1100

    feat: change document title when enter home page

--- src/HomePage/index.tsx
+++ src/HomePage/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import CustomerMenu from "../CustomerMenu";
 import InputText from "../InputText";
 import { TextContext } from "../context/text-context";
-import { useDebounce } from "../utils";
+import { useDebounce, useDocumentTitle } from "../utils";
 
 const HomePage: React.FC<{}> = () => {
   const [text, setText] = React.useState("");
   const debouncedText = useDebounce(text, 200);
+
+  useDocumentTitle("Home Page");
 
   const textValue = {
     text: debouncedText,
